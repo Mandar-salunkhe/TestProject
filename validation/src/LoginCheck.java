80e4be9c2f2cf02090a226eeaceb3a6b5a9396f9
Mandar <Mandar@ALCOHOLIC>
2020-04-16 20:00:08 +0530

    new

--- validation/src/LoginCheck.java
+++ validation/src/LoginCheck.java
@@ -40,7 +40,7 @@ public class LoginCheck extends HttpServlet {
 
 		String uname = request.getParameter("uname");
 		String password = request.getParameter("password");
-		if (uname.equals("java&database") && password.equals("123456")) {
+		if (uname.equals("java&dat") && password.equals("123456")) {
 			response.sendRedirect("member.jsp");
 		} else {
 			response.sendRedirect("error.jsp");
